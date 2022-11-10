a65f7e18db22d47f52b22f409e81db818838117b
Bruno Giarrizzo <b.giarrizzo@cyberlife.sh>
2022-11-10 16:44:04 +0100

    update homebrew script

--- script/macos/install_brew.sh
+++ script/macos/install_brew.sh
@@ -27,8 +27,7 @@ brew install gnupg
 # brew install vim --with-override-system-vi
 brew install grep
 
-# Install useful stuff
-brew install --cask alacritty
+# Install CLI stuff
 brew install htop
 brew install stow
 brew install ssh-copy-id
@@ -45,6 +44,7 @@ brew install ncdu
 brew install duf
 
 # Install Dev(ops)Stuff
+brew install --cask docker
 brew install terraform
 brew install kubernetes-cli
 brew install git-lfs
@@ -54,12 +54,45 @@ brew install helmfile
 brew install awscli
 brew install conduktor
 brew install google-cloud-sdk
-brew install gitleaks
 brew install tig
 brew install commitizen
 
+# Install Security stuff
+brew install gitleaks
+brew install nmap
+brew install wpscanteam/tap/wpscan
+
+# Install useful stuff
+brew install --cask iterm2
+brew install --cask discord
+brew install --cask transmission
+brew install --cask appcleaner
+brew install --cask vlc
+brew install --cask slack
+brew install --cask notion
+brew install --cask openemu
+brew install --cask lens
+brew install --cask visual-studio-code
+brew install --cask shadow
+brew install --cask adguard
+brew install --cask gimp
+brew install --cask google-chrome
+brew install --cask insomnia
+
 # Install SSH agent using secure enclave
-brew install secretive
+brew install --cask secretive
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+echo -e "# ------------------------------------------------ \n\n"
+echo "Install Finished"
+echo "Don't forget to install the following manually :"
+echo "  - Magnet"
+echo "  - NewsExplorer"
+echo "  - Pages"
+echo "  - Numbers"
+echo "  - Keynote"
+echo "  - Super Agent"
+echo "  - DisplayLink Manager"
+echo "  - Adguard"
