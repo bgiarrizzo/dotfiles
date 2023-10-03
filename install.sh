ab677322ff376f568eee8df72ac606cf0e188647
Bruno Giarrizzo <b.giarrizzo@cyberlife.sh>
2023-10-03 11:47:14 +0200

    update

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 if [ "$CODESPACES" ]; then
-    echo "Copying all files from dotfiles repo into ${HOME} :"
-    echo "- git"
-    rsync --exclude .git -av /workspaces/.codespaces/.persistedshare/dotfiles/git/ ${HOME}/
-    echo -e "Done \n"
-    echo "- zsh"
-    rsync --exclude .git -av /workspaces/.codespaces/.persistedshare/dotfiles/zsh/ ${HOME}/
-    echo -e "Done \n"
+    dotfiles_dir="/workspaces/.codespaces/.persistedshare/dotfiles"
 else
-    echo "Copying all files from dotfiles repo into ${HOME} :"
-    echo "- git"
-    rsync --exclude .git -av ${HOME}/dotfiles/git/ ${HOME}/
-    echo -e "Done \n"
-    echo "- zsh"
-    rsync --exclude .git -av ${HOME}/dotfiles/zsh/ ${HOME}/
-    echo -e "Done \n"
-fi
+    dotfiles_dir="${HOME}/dotfiles"
+fi;
+
+copy_file() {
+    source=$1
+    target=$2
+
+    if [ ! -e "$link" ]; then
+        echo "-----> Copying ${source} --> ${target}"
+        cp -R ${source} ${target}
+    fi
+}
+
+echo "Copying all files from dotfiles repo into ${HOME} :"
+
+for name in zsh wget vim git curl cheat; do
+    for file in $(/bin/ls -A ${dotfiles_dir}/${name}/); do
+        target="$HOME/$(basename $file)"
+        copy_file ${dotfiles_dir}/${name}/$file ${target}
+    done
+done
